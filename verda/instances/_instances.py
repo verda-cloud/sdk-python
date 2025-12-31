393689f69c7ce0f3ae12f33ec3abf461797c62b4
Alexey Shamrin <shamrin@gmail.com>
2025-12-31 15:00:02 +0200

    Update verda/instances/_instances.py

--- verda/instances/_instances.py
+++ verda/instances/_instances.py
@@ -171,8 +171,8 @@ class InstancesService:
             'description': description,
             'location_code': location,
             'os_volume': os_volume,
-            'volumes': volumes if volumes is not None else [],
-            'existing_volumes': existing_volumes if existing_volumes is not None else [],
+            'volumes': volumes or [],
+            'existing_volumes': existing_volumes or [],
             'is_spot': is_spot,
             'coupon': coupon,
         }
