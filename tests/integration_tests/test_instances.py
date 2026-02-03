f2f2aadb033260f57fc5c0102e82cc5bcff03202
Ruslan Gainutdinov <ruslan@datacrunch.io>
2026-02-03 16:03:04 +0200

    fix: tests

--- tests/integration_tests/test_instances.py
+++ tests/integration_tests/test_instances.py
@@ -1,4 +1,6 @@
+import logging
 import os
+import time
 
 import pytest
 
@@ -7,6 +9,8 @@ from verda.constants import Locations
 
 IN_GITHUB_ACTIONS = os.getenv('GITHUB_ACTIONS') == 'true'
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
 
 @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 @pytest.mark.withoutresponses
@@ -19,20 +23,35 @@ class TestInstances:
         instance = verda_client.instances.create(
             hostname='test-instance',
             location=Locations.FIN_03,
-            instance_type='CPU.4V',
-            description='test instance',
-            image='ubuntu-18.04',
+            instance_type='CPU.4V.16G',
+            description='test cpu instance',
+            image='ubuntu-22.04',
             ssh_key_ids=[ssh_key.id],
+            os_volume={'name': 'test-os-volume-cpu', 'size': 55},
         )
 
         # assert instance is created
         assert instance.id is not None
         assert instance.status == verda_client.constants.instance_status.PROVISIONING
 
-        # delete instance
-        verda_client.instances.action(instance.id, 'delete')
+        while instance.status != verda_client.constants.instance_status.RUNNING:
+            time.sleep(2)
+            logger.debug('Waiting for instance to be running... %s', instance.status)
+            instance = verda_client.instances.get_by_id(instance.id)
 
-        # permanently delete all volumes in trash
-        trash = verda_client.volumes.get_in_trash()
-        for volume in trash:
-            verda_client.volumes.delete(volume.id, is_permanent=True)
+        logger.debug('Instance is running... %s', instance.status)
+        logger.debug('Instance ID: %s', instance.id)
+        logger.debug('Instance OS Volume ID: %s', instance.os_volume_id)
+        logger.debug('Instance IP: %s', instance.ip)
+
+        # assert os volume is created
+        assert instance.os_volume_id is not None
+
+        # get os volume
+        os_volume = verda_client.volumes.get_by_id(instance.os_volume_id)
+        assert os_volume.id is not None
+        assert os_volume.name == 'test-os-volume-cpu'
+        assert os_volume.size == 55
+
+        # delete instance
+        verda_client.instances.action(instance.id, 'delete', volume_ids=[instance.os_volume_id], delete_permanently=True)
