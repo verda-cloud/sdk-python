b2d5a8c0affaa5ceb19d9fa56c41241e14caacbd
Tamir <tamir@datacrunch.io>
2026-04-16 15:06:17 +0300

    add newline after licence header

--- verda/exceptions.py
+++ verda/exceptions.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class APIException(Exception):
     """This exception is raised if there was an error from verda's API.
 
